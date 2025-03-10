df26570c351fc7cec1da31ca8b52b1a833b5cf0e
sanjana2580 <sanjanabiradar156@gmail.com>
2025-03-10 13:44:05 +0530

    2

--- infra/main.sh
+++ infra/main.sh
@@ -6,4 +6,16 @@ sudo sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 service sshd restart
 echo "worker node configuration is done"
-usermod -aG docker ansible
+sudo yum update -y
+sudo yum search docker
+sudo yum info docker
+sudo yum install -y docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo systemctl status docker.service 
+docker --version
+sudo yum install git -y
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+sudo usermod -aG docker ansible
